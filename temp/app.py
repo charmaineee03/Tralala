9f833fc336874e624b8a3fcbef9beecedb170b66
charmaineee03 <charmainegan29@gmail.com>
2025-07-10 15:08:15 +0800

    Update app.py

--- temp/app.py
+++ temp/app.py
@@ -17,3 +17,31 @@ try :
 except Exception as e: 
   st.error(f"Failed to load data: {e}")
   
+#wanna insert data into GitHub 
+">>https://github.com/settings/personal-access-tokens"
+a = st.text_input("Enter a")
+b = st.text_input("Enter b")
+c = st.text_input("Enter c") 
+
+if st.button("Upload to GitHub"):
+  df = pd.Dataframe([("a " : a, "b" : b, "c" : c)])
+  csv = df.to_csv(index=false)
+  content = base64.b64encode(csv.encode()).decode()
+
+
+url="https://api.github.com/repos/charmaineee03/temp/contents/data.csv"
+headers = ("Authorisation" :f"token {st.secrets['github']['token']}")
+
+payload = (
+  "message" : "Add data.csv",
+  "content" : content, 
+  "branch" : "main" 
+
+)
+
+r = requests.put(url, headers=headers, json=payload)
+if r.status_code in [200,201]:
+  st.success("data uploaded")
+else:
+  st.error("Failed to upload") 
+  
